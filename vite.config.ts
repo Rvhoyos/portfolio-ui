759b661e98adf4493b890c2ad32c67e4220d9047
Raul <rvhoyos@gmail.com>
2025-11-01 00:54:57 -0400

    added ui component library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 export default defineConfig({
-  plugins: [react()],
-  // keep your existing dev config as-is; this fixes *preview* only
+plugins: [react(), tailwindcss()],
+  resolve: {                                     
+    alias: { '@': path.resolve(__dirname, './src') }
+  },
+  // keep your existing dev config as-is; this fiyxes *preview* only
   preview: {
     host: true,
     port: 4173,
